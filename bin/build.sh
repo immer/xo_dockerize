76b99e64446c8edccf0a460b8839bb61f3f1ad09
Vlad Carballo <vcarballo@xogrp.com>
2016-02-09 16:16:35 -0500

    Move envs to docker image

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-docker build -f docker/Dockerfile -t wedding_api:$APP_ENV .
+docker build -f docker/Dockerfile \
+             --build-arg SECRET_KEY_BASE=$SECRET_KEY_BASE \
+             --build-arg RAILS_ENV=$RAILS_ENV \
+             --build-arg DB_HOST=$DB_HOST \
+             --build-arg DB_PORT=$DB_PORT \
+             --build-arg POSTGRES_USERNAME=$POSTGRES_USERNAME \
+             --build-arg POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+             -t wedding_api:$APP_ENV .
