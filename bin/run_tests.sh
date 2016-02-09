3532a23b282cb8f87102351563ba4e35fcb1e356
Vlad Carballo <vcarballo@xogrp.com>
2016-02-09 12:35:17 -0500

    Give postgres more time to respond

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker-compose -f docker/compose/test.yml up -d db
-sleep 3
+sleep 5
 docker-compose -f docker/compose/test.yml run --rm rspec \
   bundle exec rails db:create db:migrate
 docker-compose -f docker/compose/test.yml run --rm rspec
